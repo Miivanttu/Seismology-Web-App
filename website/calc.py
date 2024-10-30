34273eee482e629e992e180f4a2cfc59066a6110
Miivanttu <miivan@ttu.ee>
2024-10-30 18:37:15 +0200

    Calculator begining

--- website/calc.py
+++ website/calc.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 calc = Blueprint('calc', __name__)
 
-@calc.route('/calc')
-def login():
+@calc.route('/calc', methods=['GET','POST'])
+def arvuta():
+    if request.method == 'POST':
+        vonkeKiirus = request.form.get('vonkeKiirus')
+        kaugus = request.form.get('kaugus')
+        laenguMass = request.form.get('laenguMass')
+        if vonkeKiirus == None and kaugus != None and kaugus != None:
+            print(0)
+        elif vonkeKiirus != None and kaugus != None and kaugus == None:
+            print(1)
+        elif vonkeKiirus != None and kaugus == None and kaugus != None:
+            print(2)
     return render_template("calc.html")
