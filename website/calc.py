f8e6b3fbd8879eb4538f388da7bd53ba67f70a4e
Miivanttu <miivan@ttu.ee>
2024-11-18 22:01:11 +0200

    Basics remade, calc appended

--- website/calc.py
+++ website/calc.py
@@ -1,17 +1,28 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 
 calc = Blueprint('calc', __name__)
 
 @calc.route('/calc', methods=['GET','POST'])
 def arvuta():
+    result = None
     if request.method == 'POST':
         vonkeKiirus = request.form.get('vonkeKiirus')
         kaugus = request.form.get('kaugus')
         laenguMass = request.form.get('laenguMass')
-        if vonkeKiirus == None and kaugus != None and kaugus != None:
-            print(0)
-        elif vonkeKiirus != None and kaugus != None and kaugus == None:
-            print(1)
-        elif vonkeKiirus != None and kaugus == None and kaugus != None:
-            print(2)
-    return render_template("calc.html")
+
+        if vonkeKiirus == None and kaugus != None and laenguMass != None:
+            result = testFunc(0, kaugus, laenguMass)
+        elif vonkeKiirus != None and kaugus != None and laenguMass == None:
+            result = testFunc(vonkeKiirus, kaugus, 0)
+        elif vonkeKiirus != None and kaugus == None and laenguMass != None:
+            result = testFunc(vonkeKiirus, 0, laenguMass)
+        elif vonkeKiirus != None and kaugus != None and laenguMass != None:
+            result = testFunc(vonkeKiirus, kaugus, laenguMass)
+        else:
+            result = testFunc(1, 2, 5)
+    return render_template("calc.html", result = result)
+
+
+def testFunc(vonk, kaug, laen):
+    result = vonk + kaug + laen
+    return result
