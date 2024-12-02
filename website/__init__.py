de8decc70a11267521e10cd30e7bebbbf95eef47
janml <janml@LAPTOP-JACRP4N3>
2024-12-02 20:29:44 +0200

    Mingi basic andmebaasi template

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'seismika'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # SQLite database file
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -13,5 +21,8 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(calc, url_prefix='/')
     app.register_blueprint(graph, url_prefix='/')
+
+    with app.app_context():
+        db.create_all()
 
     return app
